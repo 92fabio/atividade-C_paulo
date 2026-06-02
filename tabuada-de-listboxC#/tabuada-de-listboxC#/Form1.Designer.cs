3b06ed667ce6a1d554efcaa7829049091a791992
92fabio <fabio.bala46@gmail.com>
2026-06-01 22:02:35 -0300

    tabuada de listBox

---
+++ tabuada-de-listboxC#/tabuada-de-listboxC#/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace tabuada_de_listboxC_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNumero = new System.Windows.Forms.Label();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.btnGerar = new System.Windows.Forms.Button();
+            this.listBoxTabuada = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lblNumero
+            // 
+            this.lblNumero.AutoSize = true;
+            this.lblNumero.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumero.Location = new System.Drawing.Point(311, 40);
+            this.lblNumero.Name = "lblNumero";
+            this.lblNumero.Size = new System.Drawing.Size(221, 31);
+            this.lblNumero.TabIndex = 0;
+            this.lblNumero.Text = "digite um número";
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumero.Location = new System.Drawing.Point(314, 107);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(218, 38);
+            this.txtNumero.TabIndex = 1;
+            // 
+            // btnGerar
+            // 
+            this.btnGerar.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGerar.Location = new System.Drawing.Point(317, 184);
+            this.btnGerar.Name = "btnGerar";
+            this.btnGerar.Size = new System.Drawing.Size(215, 43);
+            this.btnGerar.TabIndex = 2;
+            this.btnGerar.Text = "gerar tabuada";
+            this.btnGerar.UseVisualStyleBackColor = true;
+            this.btnGerar.Click += new System.EventHandler(this.btnGerar_Click);
+            // 
+            // listBoxTabuada
+            // 
+            this.listBoxTabuada.FormattingEnabled = true;
+            this.listBoxTabuada.Location = new System.Drawing.Point(317, 263);
+            this.listBoxTabuada.Name = "listBoxTabuada";
+            this.listBoxTabuada.Size = new System.Drawing.Size(215, 121);
+            this.listBoxTabuada.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBoxTabuada);
+            this.Controls.Add(this.btnGerar);
+            this.Controls.Add(this.txtNumero);
+            this.Controls.Add(this.lblNumero);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNumero;
+        private System.Windows.Forms.TextBox txtNumero;
+        private System.Windows.Forms.Button btnGerar;
+        private System.Windows.Forms.ListBox listBoxTabuada;
+    }
+}
+
---
+++ tabuada-de-listboxC#/tabuada-de-listboxC#/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tabuada_de_listboxC_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGerar_Click(object sender, EventArgs e)
+        {
+            Text = "Gerando tabuada...";
+
+            int numero = Convert.ToInt32(txtNumero.Text);
+
+            listBoxTabuada.Items.Clear();
+
+            for (int i = 1; i <= 10; i++)
+            {
+                listBoxTabuada.Items.Add(
+                    numero + " x " + i + " = " + (numero * i)
+                );
+            } 
+        }
+
+    }
+}
