05976fea50a0c81608e869771fe3e2c5b8c0d180
92fabio <fabio.bala46@gmail.com>
2026-06-01 22:10:46 -0300

    interface de login

---
+++ atividade-paraC#/atividade-paraC#/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace atividade_paraC_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textNomeUsuario = new System.Windows.Forms.TextBox();
+            this.textSenhaUsuario = new System.Windows.Forms.TextBox();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(93, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(161, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome do usuário:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(93, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(164, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Senha do usuário:";
+            // 
+            // textNomeUsuario
+            // 
+            this.textNomeUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textNomeUsuario.Location = new System.Drawing.Point(281, 60);
+            this.textNomeUsuario.Name = "textNomeUsuario";
+            this.textNomeUsuario.Size = new System.Drawing.Size(225, 29);
+            this.textNomeUsuario.TabIndex = 2;
+            // 
+            // textSenhaUsuario
+            // 
+            this.textSenhaUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textSenhaUsuario.Location = new System.Drawing.Point(281, 118);
+            this.textSenhaUsuario.Name = "textSenhaUsuario";
+            this.textSenhaUsuario.Size = new System.Drawing.Size(225, 29);
+            this.textSenhaUsuario.TabIndex = 3;
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalvar.Location = new System.Drawing.Point(97, 195);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(409, 88);
+            this.btnSalvar.TabIndex = 4;
+            this.btnSalvar.Text = "salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.textSenhaUsuario);
+            this.Controls.Add(this.textNomeUsuario);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textNomeUsuario;
+        private System.Windows.Forms.TextBox textSenhaUsuario;
+        private System.Windows.Forms.Button btnSalvar;
+    }
+}
+
---
+++ atividade-paraC#/atividade-paraC#/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace atividade_paraC_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+
+            Text = "Faça seu login!!! ";
+
+            var nomeUsuario = textNomeUsuario.Text;
+            var senhaUsuario = textSenhaUsuario.Text;
+
+
+            if (nomeUsuario == "fabio furtado" && senhaUsuario == "1234")
+            {
+                MessageBox.Show("Login realizado com sucesso...");
+            }
+            else
+            {
+                MessageBox.Show("Algo deu errado");
+            } 
+        }
+    }
+}
